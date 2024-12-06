87432e605bdbd1ff0b5db4a552ecc0d69d76dee4
Lucas_Hanz <92007053+luchlima@users.noreply.github.com>
2024-12-05 22:02:46 -0300

    Some changes/4.

--- src/br/com/projeto/modelos/Opcoes.java
+++ src/br/com/projeto/modelos/Opcoes.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Opcoes {
 
-    private DepositoValor depositoValor;
+    private final DepositoValor depositoValor;
     public Opcoes(DepositoValor depositoValor){
         this.depositoValor = depositoValor;
     }
 
     public void escolherOpcao(){
         Scanner leitura = new Scanner(System.in);
-        double saldoAtual;
+
         System.out.println("""
                             ==========||==========||==========||
                                         Operações:
@@ -25,6 +25,7 @@ public class Opcoes {
 
         boolean opcaoDigitada = false;
 
+        double saldoAtual = depositoValor.getSaldoAtual();
         while (!opcaoDigitada){
 
             int escolhido = leitura.nextInt();
@@ -65,10 +66,51 @@ public class Opcoes {
                         saldoAtual = depositoValor.getSaldoAtual() + valorRecebido;
                         System.out.println("Recebimento confirmado com sucesso!");
                         System.out.println("Seu saldo atual é de R$ " + saldoAtual);
+                        System.out.println("""
+                            ==========||==========||==========||
+                                    Deseja voltar para as
+                                           opções?
+                                           1 - Sim
+                                           2 - Não
+                            ==========||==========||==========||
+                            """);
+                        int opcaoEscolhidaVoltar = leitura.nextInt();
+                        if (opcaoEscolhidaVoltar == 1){
+                            escolherOpcao();
+                        } else {
+                            System.out.println("Banco LG agradece a sua visita!");
+                            System.out.println("Encerrando aplicação...");
+                        }
+
+                    } else {
+                        System.out.println("Você pode tentar novamente!");
                     }
-
-
                 case 3:
+                    System.out.println("Digite o valor a ser transferido: ");
+                    double valorTransferido = leitura.nextDouble();
+                    System.out.println("Tem certeza que deseja transferir R$ " + valorTransferido + "?");
+
+                    System.out.println("""
+                                ==========||==========||
+                                        1 - Sim
+                                        2 - Não
+                                ==========||==========||
+                                """);
+                    int opcaoTransferir = leitura.nextInt();
+                    if (opcaoTransferir == 1) {
+                        if (valorTransferido <= saldoAtual) {
+                            saldoAtual -= valorTransferido;
+                            System.out.println("Valor transferido com sucesso!");
+                            System.out.println("Saldo Atual é de R$ " + saldoAtual);
+                        } else {
+                            System.out.println("Saldo insuficiente para realizar a transferência.");
+                        }
+                    } else if (opcaoTransferir == 2) {
+                        System.out.println("Voltando ao menu de opções...");
+                        escolherOpcao();
+                    }else{
+                        System.out.println("Opção Invalida, tente novamente!");
+                    }
 
                 case 4:
 
